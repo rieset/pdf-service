8b44d938243984fbd5f048b986c6e6b7a313fab8
Albert Iblyaminov <albert@iblyaminov.io>
2020-10-10 01:59:54 +0300

    Added project files

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,31 @@
-import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
-import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest'
+import { Test } from '@nestjs/testing'
+import { AppModule } from './../src/app.module'
+import { ConfigModule } from '@nestjs/config'
+import { HealthController } from '../src/health/health.controller'
+import { PdfController } from '../src/pdf/pdf.controller'
+import { PdfService } from '../src/pdf/pdf.service'
+import { TerminusModule } from '@nestjs/terminus'
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      imports: [AppModule, TerminusModule, ConfigModule.forRoot()],
+      controllers: [HealthController, PdfController],
+      providers: [PdfService]
+    }).compile()
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+    const app = moduleFixture.createNestApplication()
+    await app.init()
+  })
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+  it('/ready (GET)', () => {
+    return request(app.getHttpServer()).get('/ready').expect(200)
+  })
+
+  it('/health (GET)', () => {
+    return request(app.getHttpServer()).get('/health').expect(200)
+  })
+})
